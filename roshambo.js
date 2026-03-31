b877234e95f454d8a19c2db4292eedf6b2b8b821
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-03-31 09:55:11 -0500

    Add function for playing round

--- roshambo.js
+++ roshambo.js
@@ -14,8 +14,12 @@ function startGame() {
     playerScore = 0;
     computerScore = 0;
     round = 1;
-    startGameMessage();     
-    startRound();
+    startGameMessage(); 
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
 }
 /*
 Print message stating game is beginning
@@ -34,21 +38,21 @@ function startGameMessage() {
 startGame();
 
 
-/*Start round startRound()
+/*Start round playRound()
     Print message stating round and current score, then prompt user for their choice
     PRINT: "Round: <round>. The score is: "Human": userScore() - Computer: computerScore()"
 */
 function startRound() {
     console.log("Round:" + round);
     showScore();
-    getHumanChoice();
-    getComputerChoice();
 }
 
 function showScore() {
     console.log("Player | Computer");
     console.log("   " + playerScore + "   -    " + computerScore);
 }
+
+
 
 /*
 Prompt for input
@@ -65,39 +69,84 @@ function promptUser() {
     return prompt(promptText);
 }
 
+//Assignment assumes a correct choice will be entered; no input validation yet.
 function getHumanChoice() {
-    let humanChoice = promptUser();
-    //TODO: remove this log, for debugging
-    console.log("User selected " + humanChoice);
-    
-}
-
-function getComputerChoice() {
-    //TODO: remove this log, for debugging
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
-    console.log("Computer selected " + computerChoice);
+    let humanChoice = parseInt(promptUser());
+    return convertChoice(humanChoice);
 }
 
 
 /*
-Get the user choice
-    prompt() to receive input
-    Validate user input
-        FUNC: validateChoice()
-                IF: user inputs valid choice (number 1 to 3), TRUE - continue with game
-                    - getComputerChoice()
-                ELSE: user inputs invalid choice, FALSE - Reprompt for choice
-
 Randomize computer choice for each round
     FUNC: getComputerChoice()
     - Math.Random()
     - Random number from 1 to 3
     - 1 = Rock, 2 = Paper, 3 = Scissors
+*/
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    return convertChoice(computerChoice);
+}
+
+//helper function to convert number to string
+function convertChoice(integer) {
+    if(integer === 1) {
+        return "Rock";
+    }
+    if(integer === 2) {
+        return "Paper";
+    }
+    if(integer === 3) {
+        return "Scissors";
+    }
+}
+
+function playRound(humanSelection, computerSelection) {
+    startRound();
+
+    //tie condition
+    if(humanSelection === computerSelection) {
+        console.log("TIE!")
+    }
+
+    //human winning conditions
+    if(humanSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("Rock beats Scissors! You win this round");
+        playerScore++;
+    }
+    if(humanSelection === "Paper" && computerSelection === "Rock") {
+        console.log("Paper beats Rock! You win this round");
+        playerScore++;
+    }
+    if(humanSelection === "Scissors" && computerSelection === "Paper") {
+    console.log("Scissors beats Paper! You win this round");
+    playerScore++;
+    }
+
+    //computer winning conditions
+    if(humanSelection === "Rock" && computerSelection === "Paper") {
+        console.log("Rock loses to Paper! You lose this round");
+        computerScore++;
+    }
+    if(humanSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("Paper loses to Scissors! You lose this round");
+        computerScore++;
+    }
+    if(humanSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("Scissors loses to Rock! You lose this round");
+        computerScore++;
+    }
+    round++;
+}
+
+
+/*
+
 
 Evaluate round evaluateResults()
     Check if tie.
         Increment Round #
-        Start new round startRound()
+        Start new round playRound()
     If not tie
         update score for winner
             check if score goal reached
@@ -111,5 +160,15 @@ Evaluate round evaluateResults()
                             anything else - reprompt playAgain
                 ELSE 
                     increment round#
-                    start new round startRound()
+                    start new round playRound()
+
+
+Game Logic
+Choices are the same = tie; no change to score
+Player chooses rock, computer chooses scissors = Player wins, increment score
+Player chooses rock, computer chooses paper = Player loses, increment computer score
+Player chooses scissors, computer chooses paper = Player wins, increment player score
+Player chooses scissors, computer chooses rock = player loses, increment computer
+Player chooses paper, computer chooses rock = player wins, increment player
+Player chooses paper, computer chooses scissors, player loses, increment computer
 */
