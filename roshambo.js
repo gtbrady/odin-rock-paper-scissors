c171f9fda26bf769e8c7c9ba9b9beae97b28879f
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-05-01 09:53:32 -0500

    Update so results display on page
    Removed all printing to console and moved results so they display on the
    web page. Also removed any functions that are no longer needed with
    the refactor

--- roshambo.js
+++ roshambo.js
@@ -12,6 +12,18 @@ const scoreboardHeader = document.querySelector("#scoreboardHeader");
 scoreboardHeader.innerText = `Round: ${round}\nPlayer | Computer`;
 const scoreboard = document.querySelector("#scoreboard");
 scoreboard.innerText = `${playerScore} - ${computerScore}`;
+const results = document.querySelector("#results");
+
+
+/*
+TODO
+Add Play Game button
+- disable 3 main buttons to start
+- play game enables buttons and starts the game (resets rounds/score)
+- play game button is disabled
+- once score target is reached, game over. Disable buttons again. Re-enable play game button
+
+*/
 
 
 
@@ -25,43 +37,15 @@ function startGame() {
     playerScore = 0;
     computerScore = 0;
     round = 1;
-    playGame();
-    exitGame();
-}
-
-function playGame() {
-    while(playerScore < 5 && computerScore < 5) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
 }
 
 //initialize game:
 // startGame();
 
-
-//display score
-function showScore() {
-    //UI UPDATE add div so this displays in browser vs console
-    console.log("Player | Computer");
-    console.log("   " + playerScore + "   -    " + computerScore);
-}
-
-
-function promptUser() {
-    /*UI UPDATE add div so this displays in browser vs console
-    Need to change input method from prompt() to button click/event listener
-
-    */
-    let promptText = "Select a number from 1 to 3:\n1 for Rock\n2 for Paper\n3 for Scissors"
-    return prompt(promptText);
-}
-
 function buttonClick(event) {
     playRound(event.target.innerText,getComputerChoice());
 }
 
-//Assignment assumes a correct choice will be entered; no input validation yet.
 function getHumanChoice() {
     let humanChoice = parseInt(promptUser());
     return convertChoice(humanChoice);
@@ -91,59 +75,54 @@ function playRound(humanSelection, computerSelection) {
 
     //tie condition
     if(humanSelection === computerSelection) {
-        console.log("TIE!")
+        results.innerText = "TIE!";
     }
 
     //human winning conditions
     if(humanSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("Rock beats Scissors! You win this round");
+        results.innerText = "Rock beats Scissors! You win this round"
         playerScore++;
     }
     if(humanSelection === "Paper" && computerSelection === "Rock") {
-        console.log("Paper beats Rock! You win this round");
+        results.innerText = "Paper beats Rock! You win this round"
         playerScore++;
     }
     if(humanSelection === "Scissors" && computerSelection === "Paper") {
-    console.log("Scissors beats Paper! You win this round");
-    playerScore++;
+        results.innerText = "Scissors beats Paper! You win this round"
+        playerScore++;
     }
 
     //computer winning conditions
     if(humanSelection === "Rock" && computerSelection === "Paper") {
-        console.log("Rock loses to Paper! You lose this round");
+        results.innerText = "Rock loses to Paper! You lose this round"
         computerScore++;
     }
     if(humanSelection === "Paper" && computerSelection === "Scissors") {
-        console.log("Paper loses to Scissors! You lose this round");
+        results.innerText = "Paper loses to Scissors! You lose this round"
         computerScore++;
     }
     if(humanSelection === "Scissors" && computerSelection === "Rock") {
-        console.log("Scissors loses to Rock! You lose this round");
+        results.innerText = "Scissors loses to Rock! You lose this round"
         computerScore++;
     }
     round++;
     updateScoreboard();
-    showScore();
+    evaluateWinner();
+
 }
 
-/*UI UPDATE:
-Need to rework this probably into 2 separate functions
-need to check scores at end of round to see if victory condition met
-if it has, then evaluate winner
-
-*/
 function evaluateWinner() {
-    if(playerScore > computerScore) {
-        console.log("You win! Congratulations!");
-    } else if(computerScore > playerScore){
-        console.log("The computer wins... Better luck next time.")
-    } else {
-        console.log("The game ended in a tie!")
+    let gameOver = false;
+    if(playerScore === 5) {
+        results.innerText = "You Won!"
+        gameOver = true;
+    }
+    if(computerScore === 5) {
+        results.innerText = "You Lost!"
+        gameOver = true;
     }
 }
 
-function exitGame() {
-    console.log("Final Result:");
-    showScore();
-    evaluateWinner();
+function toggleButtons() {
+
 }
